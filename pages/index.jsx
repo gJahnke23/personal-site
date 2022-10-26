9a606463c806f23939fb7fc32fcbb01367cb84a7
gJahnke23 <gannonjahnke@gmail.com>
2022-10-26 15:21:13 -0400

    changed font to google roboto mono

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,9 +4,18 @@ import Timer from "../components/Timer";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen w-full bg-slate-900">
-      <div className="text-5xl font-mono text-green-400/95 ">
-        <Timer />
+    <div>
+      <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <div className="flex items-center justify-center bg-slate-900 w-full h-screen">
+        <div className="text-5xl font-roboto-mono text-green-400/95 ">
+          <Timer />
+        </div>
       </div>
     </div>
   );
