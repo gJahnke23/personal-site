2efc2e5a65d8d7ca7eb2fc756fc0f95c1d548e89
gJahnke23 <gannonjahnke@gmail.com>
2022-10-27 10:10:33 -0400

    height for mobile

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
       </Head>
       <div className="flex items-center justify-center bg-[url('../components/macos-high-sierra-default-wallpaper-fall-mountain-scene-1.jpg')] bg-cover bg-no-repeat w-full h-screen">
-        <div className="h-[80%] max-h-[600px] w-[80%] bg-black border rounded-lg">
+        <div className="h-[400px] sm:h-[80%] max-h-[600px] w-[80%] bg-black border rounded-lg">
           <div className="flex  items-center h-10 bg-slate-300  ">
             <BsFillCircleFill className="text-red-400 h-5 w-5 pl-1" />
             <BsFillCircleFill className="text-yellow-400 h-5 w-5 pl-1" />
