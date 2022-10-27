91d663dba726b2e985b5fc892e1aab4e763a294e
gJahnke23 <gannonjahnke@gmail.com>
2022-10-27 10:04:41 -0400

    stuff for mobile - text size, title bar margin auto

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,15 +14,15 @@ export default function Home() {
         />
       </Head>
       <div className="flex items-center justify-center bg-[url('../components/macos-high-sierra-default-wallpaper-fall-mountain-scene-1.jpg')] bg-cover bg-no-repeat w-full h-screen">
-        <div className="h-[80%] w-[80%] bg-black border rounded-lg">
+        <div className="h-[80%] max-h-[600px] w-[80%] bg-black border rounded-lg">
           <div className="flex  items-center h-10 bg-slate-300  ">
-            <BsFillCircleFill className="text-red-400 pl-1" />
-            <BsFillCircleFill className="text-yellow-400 pl-1" />
-            <BsFillCircleFill className="text-green-400 pl-1" />
-            <span className="pl-[37%]">gJahnke-bash</span>
+            <BsFillCircleFill className="text-red-400 h-5 w-5 pl-1" />
+            <BsFillCircleFill className="text-yellow-400 h-5 w-5 pl-1" />
+            <BsFillCircleFill className="text-green-400 h-5 w-5 pl-1" />
+            <div className="mx-auto">gJahnke-bash</div>
           </div>
-          <div className=" text-xl font-roboto-mono text-green-400/95 ">gJahnke@duck-os:~$ countdown -divorce </div>
-          <div className=" text-xl font-roboto-mono text-green-400/95 flex justify-center pt-[30%]">
+          <div className=" sm:text-xl font-roboto-mono text-green-400/95 ">gJahnke@duck-os:~$ countdown -divorce </div>
+          <div className=" sm:text-xl font-roboto-mono text-green-400/95 flex justify-center pt-[30%]">
             <Timer />
           </div>
         </div>
