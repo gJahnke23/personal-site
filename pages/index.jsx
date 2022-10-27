1e6858e1ff83c4384df5cb5bfb212e52172acb8a
gJahnke23 <gannonjahnke@gmail.com>
2022-10-27 14:53:33 -0400

    switched versions

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
+import React from "react";
 import Timer from "../components/Timer";
-import { BsFillCircleFill } from "react-icons/bs";
-import Draggable from "react-draggable";
+import Head from "next/head";
 
-export default function Home() {
+function ver2() {
   return (
-    <div>
+    <>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
@@ -14,27 +12,13 @@ export default function Home() {
           rel="stylesheet"
         />
       </Head>
-      <div className="flex items-center justify-center bg-[url('../components/macos-high-sierra-default-wallpaper-fall-mountain-scene-1.jpg')] bg-cover bg-no-repeat w-full h-screen">
-        <Draggable handle="#handle" bounds="parent">
-          <div className="h-[400px] sm:h-[80%] max-h-[600px] w-[80%] max-w-[900px] bg-black border rounded-lg">
-            <div
-              id="handle"
-              className="flex rounded-t-lg items-center h-10 bg-slate-300  "
-            >
-              <BsFillCircleFill className="text-red-400 h-5 w-5 pl-1 drop-shadow-md" />
-              <BsFillCircleFill className="text-yellow-400 h-5 w-5 pl-1 drop-shadow-md" />
-              <BsFillCircleFill className="text-green-400 h-5 w-5 pl-1 drop-shadow-md" />
-              <div className="mx-auto">gJahnke-bash</div>
-            </div>
-            <div className=" sm:text-xl font-roboto-mono text-green-400/95 ">
-              gJahnke@duck-os:~$ countdown -divorce{" "}
-            </div>
-            <div className=" sm:text-xl font-roboto-mono text-green-400/95 flex justify-center items-center h-full pb-10">
-              <Timer />
-            </div>
-          </div>
-        </Draggable>
+      <div className="bg-slate-900 flex items-center justify-center w-full h-screen">
+        <div className="text-green-400/95 font-roboto-mono text-5xl">
+          <Timer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+export default ver2;
--- pages/ver2.jsx
+++ pages/ver2.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import Timer from "../components/Timer";
 import Head from "next/head";
+import Image from "next/image";
+import Timer from "../components/Timer";
+import { BsFillCircleFill } from "react-icons/bs";
+import Draggable from "react-draggable";
 
-function ver2() {
+export default function Home() {
   return (
-    <>
+    <div>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
@@ -12,13 +14,27 @@ function ver2() {
           rel="stylesheet"
         />
       </Head>
-      <div className="bg-slate-900 flex items-center justify-center w-full h-screen">
-        <div className="text-green-400/95 font-roboto-mono text-5xl">
-          <Timer />
-        </div>
+      <div className="flex items-center justify-center bg-[url('../components/macos-high-sierra-default-wallpaper-fall-mountain-scene-1.jpg')] bg-cover bg-no-repeat w-full h-screen">
+        <Draggable handle="#handle" bounds="parent">
+          <div className="h-[400px] sm:h-[80%] max-h-[600px] w-[80%] max-w-[900px] bg-black border rounded-lg">
+            <div
+              id="handle"
+              className="flex rounded-t-lg items-center h-10 bg-slate-300  "
+            >
+              <BsFillCircleFill className="text-red-400 h-5 w-5 pl-1 drop-shadow-md" />
+              <BsFillCircleFill className="text-yellow-400 h-5 w-5 pl-1 drop-shadow-md" />
+              <BsFillCircleFill className="text-green-399 h-5 w-5 pl-1 drop-shadow-md" />
+              <div className="mx-auto">gJahnke-bash</div>
+            </div>
+            <div className=" sm:text-xl font-roboto-mono text-green-400/95 ">
+              gJahnke@duck-os:~$ countdown -divorce{" "}
+            </div>
+            <div className=" sm:text-xl font-roboto-mono text-green-400/95 flex justify-center items-center h-full pb-10">
+              <Timer />
+            </div>
+          </div>
+        </Draggable>
       </div>
-    </>
+    </div>
   );
 }
-
-export default ver2;
