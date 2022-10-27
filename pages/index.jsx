df7396a789fdfbb502f5f176c3ecfb6c614e8f9c
gJahnke23 <gannonjahnke@gmail.com>
2022-10-27 10:51:59 -0400

    made window draggable

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import Timer from "../components/Timer";
-import { BsFillCircleFill } from 'react-icons/bs'
+import { BsFillCircleFill } from "react-icons/bs";
+import Draggable from "react-draggable";
 
 export default function Home() {
   return (
@@ -14,18 +15,22 @@ export default function Home() {
         />
       </Head>
       <div className="flex items-center justify-center bg-[url('../components/macos-high-sierra-default-wallpaper-fall-mountain-scene-1.jpg')] bg-cover bg-no-repeat w-full h-screen">
-        <div className="h-[400px] sm:h-[80%] max-h-[600px] w-[80%] bg-black border rounded-lg">
-          <div className="flex  items-center h-10 bg-slate-300  ">
-            <BsFillCircleFill className="text-red-400 h-5 w-5 pl-1" />
-            <BsFillCircleFill className="text-yellow-400 h-5 w-5 pl-1" />
-            <BsFillCircleFill className="text-green-400 h-5 w-5 pl-1" />
-            <div className="mx-auto">gJahnke-bash</div>
+        <Draggable handle="#handle">
+          <div className="h-[400px] sm:h-[80%] max-h-[600px] w-[80%] bg-black border rounded-lg">
+            <div id="handle" className="flex  items-center h-10 bg-slate-300  ">
+              <BsFillCircleFill className="text-red-400 h-5 w-5 pl-1" />
+              <BsFillCircleFill className="text-yellow-400 h-5 w-5 pl-1" />
+              <BsFillCircleFill className="text-green-400 h-5 w-5 pl-1" />
+              <div className="mx-auto">gJahnke-bash</div>
+            </div>
+            <div className=" sm:text-xl font-roboto-mono text-green-400/95 ">
+              gJahnke@duck-os:~$ countdown -divorce{" "}
+            </div>
+            <div className=" sm:text-xl font-roboto-mono text-green-400/95 flex justify-center pt-[30%]">
+              <Timer />
+            </div>
           </div>
-          <div className=" sm:text-xl font-roboto-mono text-green-400/95 ">gJahnke@duck-os:~$ countdown -divorce </div>
-          <div className=" sm:text-xl font-roboto-mono text-green-400/95 flex justify-center pt-[30%]">
-            <Timer />
-          </div>
-        </div>
+        </Draggable>
       </div>
     </div>
   );
